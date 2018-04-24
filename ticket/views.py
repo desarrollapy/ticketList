2e5cead29356b71b48a2e6ff9657612740b32391
juanrybritez <juanrybritez@gmail.com>
2018-04-23 23:01:35 -0400

    Fix: Paginas con login required

--- ticket/views.py
+++ ticket/views.py
@@ -18,28 +18,22 @@ def login_page(request):
         Vista para ingresar y autenticarse, para poder utilizar el sistema.
 
     """
-
     if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
 
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    print "Entrooo"
-                    return HttpResponseRedirect('/inicio')
-                else:
-                    print "Usuario inactivo."
-                    messages.add_message(request, messages.WARNING, 'Usuario inactivo.')
+        username = request.POST.get('usuario')
+        password = request.POST.get('password')
+
+        print(username)
+        print(password)
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/inicio')
             else:
-                print "Nombre de usuario y/o password incorrecto."
-                messages.add_message(request, messages.ERROR, 'Nombre de usuario y/o password incorrecto.')
+                messages.add_message(request, messages.WARNING, 'Usuario inactivo.')
         else:
-            messages.add_message(request, messages.WARNING, 'Datos invalidos.')
-    else:
-        form = LoginForm()
+            messages.add_message(request, messages.ERROR, 'Nombre de usuario y/o password incorrecto.')
 
     return render(request, 'login.html')
